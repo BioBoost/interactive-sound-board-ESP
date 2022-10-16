021e7588652107fb69b9734974da63f97b0b5f4f
r0755466 <r0755466@gmail.com>
2022-10-16 23:17:23 +0200

    Code added, instructions added

---
+++ Mqqt-ESP1/Mqqt-ESP1.ino
@@ -0,0 +1,124 @@
+
+#include <WiFi.h>
+#include "PubSubClient.h" // Connect and publish to the MQTT broker
+
+#define TRIG_PIN 0 // ESP32 pin GIOP23 connected to Ultrasonic Sensor's TRIG pin
+#define ECHO_PIN 1 // ESP32 pin GIOP22 connected to Ultrasonic Sensor's ECHO pin
+
+// Code ESP1
+
+
+// SENSOR
+float duration_us, distance_cm;
+
+// WIFI netwerk
+// Network on school  
+//const char* ssid      = "devbit-verhuis";
+//const char* password  = "Dr@@dloos!";
+
+// Network home
+const char* ssid      = "WiFi-2.4-FA30";
+const char* password  = "wzsd7cyh5e76a";
+
+
+
+// MQTT 
+const char* mqtt_server = "mqtt.devbit.be";  // Broker we connect to 
+const char* sensor_topic = "test/soundboard/esp1"; // Topic we send to
+const char* mqtt_username = "test_esp1"; // MQTT username
+const char* mqtt_password = "test_esp1"; // MQTT password
+const char* clientID = "client_soundboard_esp1"; // MQTT client ID
+
+
+// Initialise the WiFi and MQTT Client objects
+
+WiFiClient wifiClient;
+
+// 1883 is the listener port for the Broker
+
+PubSubClient client(mqtt_server, 1883, wifiClient); 
+
+
+
+
+void connect_MQTT(){
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  // Connect to the WiFi
+  WiFi.begin(ssid, password);
+
+  // Wait until the connection has been confirmed before continuing
+  while (WiFi.status() != WL_CONNECTED) {
+    // to test 1s , otherwise 500ms
+    delay(1000);
+    Serial.print(".");
+  }
+
+  // Connect to MQTT Broker
+  // client.connect returns a boolean value to let us know if the connection was successful.
+  // If the connection is failing, make sure you are using the correct MQTT Username and Password (Setup Earlier in the Instructable)
+  /*
+  if (client.connect(clientID, mqtt_username, mqtt_password)) {
+    Serial.println("Connected to MQTT Broker!");
+  }
+  */
+  if (client.connect(clientID, mqtt_username, mqtt_password)) {
+    Serial.println("Connected to MQTT Broker!");
+  }
+  else {
+    Serial.println("Connection to MQTT Broker failed...");
+  }
+}
+
+
+
+void setup() {
+  // begin serial port
+  Serial.begin (9600);
+  // configure the trigger pin to output mode
+  pinMode(TRIG_PIN, OUTPUT);
+  // configure the echo pin to input mode
+  pinMode(ECHO_PIN, INPUT);
+
+  connect_MQTT();
+}
+
+void loop() {
+  // generate 10-microsecond pulse to TRIG pin
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // measure duration of pulse from ECHO pin
+  duration_us = pulseIn(ECHO_PIN, HIGH);
+
+   // To test home without using the sensor 
+   float max = 100; 
+   float min = 100;
+   random(max, min);
+
+   
+  // calculate the distance
+  //distance_cm = 0.017 * duration_us;
+
+  
+  // For testing from home 
+  distance_cm = 100 +rand()%200;
+  // We publich the data to the topic
+  if (client.publish(sensor_topic, String(distance_cm).c_str())) {
+    Serial.println("distance sent!");
+  }
+  // Again, client.publish will return a boolean value depending on whether it succeded or not.
+  // If the message failed to send, we will try again, as the connection may have broken.
+
+
+
+  // print the value to Serial Monitor
+  Serial.print("distance: ");
+
+
+ 
+  Serial.print(distance_cm);
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# interactive-sound-board-ESP
+# Interactive-sound-board-ESP
+Arduino code of all the ESP.
+We are using the model: ESP32-C3-DevKitM-1
+
+## Execute
+Require to install needed libraries:
+Installing ESP32 Add-on in Arduino IDE
+
+We need to add on preferances Additional Board Manager URLs:
+https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+
+Then in board manager we can type: ESP32
+We install the ESP32 by Espressif Systems
+Than we chose ur board: In tools/board/esp32
+
+## Important parts of the code
+
+### We take as example the Code of EXP1:
+
+The needed ssid and password to connect to access point
+
+```CPP
+const char* ssid      = "devbit-verhuis";
+const char* password  = "Dr@@dloos!";
+```
+
+We connect to the broker mqtt.devbit.be, we publisch to
+test/soundboard/esp1. We declare an client ID soo the socket knows what unical client it is.
+
+```CPP
+const char* mqtt_server = "mqtt.devbit.be";
+const char* sensor_topic = "test/soundboard/esp1"; 
+const char* mqtt_username = "test_esp1"; 
+const char* mqtt_password = "test_esp1"; 
+const char* clientID = "client_soundboard_esp1"; 
+```
+
+```CPP
+const char* mqtt_server = "mqtt.devbit.be";
+const char* sensor_topic = "test/soundboard/esp1"; 
+const char* mqtt_username = "test_esp1"; 
+const char* mqtt_password = "test_esp1"; 
+const char* clientID = "client_soundboard_esp1"; 
+```
+
+Inside **void Setup** we publish ur value to the socket. We convert the float in a type string.
+
+```CPP
+ if (client.publish(sensor_topic, String(distance_cm).c_str())) {
+    Serial.println("distance sent!");
+  }
+```
+
+We have tree modules and every module publish a value to an topic.
+
+![alt text](./img/Basic-mqtt.png "label")
