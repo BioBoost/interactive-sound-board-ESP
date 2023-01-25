7097e3261834a083d9d6a05559075b1a836c1bc3
Nico De Witte <nico.dewitte@outlook.com>
2023-01-25 22:49:29 +0100

    Start of refactor for innovation market

--- Mqqt-ESP/Mqqt-ESP.ino
+++ Mqqt-ESP/Mqqt-ESP.ino
@@ -1,6 +1,24 @@
 #include <WiFi.h>
 #include "PubSubClient.h" 
 #include "Freenove_WS2812_Lib_for_ESP32.h"
+#include <ArduinoJson.h>
+
+const char * WIFI_SSID = "xxxxxxxxxxxxxx";
+const char * WIFI_PASSWORD = "xxxxxxxxxxxxxxx";
+const char * MQTT_BROKER_IP = "xxxxxxxxxxxxxx";
+const unsigned int MQTT_BROKER_PORT = 1883;
+const String MQTT_BASE_TOPIC = "test/sonic/modules";
+
+// HS SR04 Ultrasonic Sensor Pins
+#define TRIGGER_PIN 0
+#define ECHO_PIN    1
+
+// Communications
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient); 
+
+String mqttClientId = "";
+
 
 
 #define LEDS_COUNT  1
@@ -10,27 +28,15 @@
 // RGB LED lib
 Freenove_ESP32_WS2812 strip = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
 
-#define TRIG_PIN 0 // ESP32 pin GIOP23 connected to Ultrasonic Sensor's TRIG pin
-#define ECHO_PIN 1 // ESP32 pin GIOP22 connected to Ultrasonic Sensor's ECHO pin
+
 // SENSOR
 float duration_us, distance_cm;
 float distance_save; 
 float * ptr_distance_save;
 
-// WIFI netwerk
-// Network on school  
-const char* ssid      = "SoundBoardAP";
-const char* password  = "123456789PiPi";
-
 // MQTT 
-const char* mqtt_server = "192.168.4.18";  // Broker we connect to s
 const char * devices_topic = "test/devices/";
 
-// Initialise the WiFi and MQTT Client objects
-WiFiClient wifiClient;
-
-// 1883 is the listener port for the Broker
-PubSubClient client(mqtt_server, 1883, wifiClient); 
 
 //globale string variable ! niet aanpassen.
 String globalClientID;
@@ -56,34 +62,58 @@ String samen_ = String(status_s + WiFi.macAddress() + "/status");
 // Char convert from string 
 const char * sensor_topic_status = samen_.c_str();
 
-void connect_WIFI_MQTT(){
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
 
-  // Connect to the WiFi
-  WiFi.begin(ssid, password);
+void status_no_communications(void) {
+  strip.setLedColorData(0,255,0,0);
+  strip.show();
+}
 
-  // Wait until the connection has been confirmed before continuing
+void status_only_wifi(void) {
+  strip.setLedColorData(0, 255, 255, 0);
+  strip.show();
+}
+
+void status_wifi_and_mqtt(void) {
+  strip.setLedColorData(0, 0, 255, 0);
+  strip.show();
+}
+
+bool connect_to_wifi(void) {
+  Serial.print("Connecting to WiFi ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
   while (WiFi.status() != WL_CONNECTED) {
-    // to test 1s , otherwise 500ms
     delay(1000);
-    Serial.print(".");
+    Serial.println("Not yet connected ...");
   }
-  
-  String clientID = WiFi.macAddress().substring(15) + "_esp_sound_" + String(random(1000,9999));
-  globalClientID = clientID;
-  if (client.connect(clientID.c_str())) {
-    Serial.println("Connected to MQTT Broker!");
-    Serial.println(clientID);
-    strip.setLedColorData(0, 255, 0, 0);
-    strip.show();
-    
+
+  Serial.println("Connected to WiFi");
+  return true;
+}
+
+String generate_client_id(void) {
+  static long randomId = random(1000,9999);
+  String mqttClientId = WiFi.macAddress().substring(9);
+  mqttClientId.replace(":", "");
+  mqttClientId.toLowerCase();
+  mqttClientId += "-esp32-sonic-" + String(randomId);
+  return mqttClientId;
+}
+
+bool connect_to_mqtt_broker(void) {
+  mqttClient.setServer(MQTT_BROKER_IP, MQTT_BROKER_PORT);
+
+  if (mqttClient.connect(generate_client_id().c_str())) {
+    Serial.print("Connected to MQTT Broker as ");
+    Serial.println(generate_client_id());
   }
   else {
-    Serial.println("Connection to MQTT Broker failed...");
-    strip.setLedColorData(0,0, 0, 255);
-    strip.show();
+    Serial.println("Connection to MQTT Broker failed ..");
   }
+
+  return mqttClient.connected();
 }
 
 // ********* callback 
@@ -111,107 +141,77 @@ void callback(char *topic, byte *payload, unsigned int length) {
  Serial.println("-----------------------");
 }
 
-void reconnect_MQTT() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection... ");
-    // Attempt to connect
-    Serial.println(globalClientID.c_str());
-    if (client.connect(globalClientID.c_str())) {
-      Serial.println("connected");
-      // Subscribe
-      client.subscribe(sensor_topic_status);
-      Serial.println(sensor_topic_status);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
+void publish_hello_package(void) {
+  String helloTopic = MQTT_BASE_TOPIC + "/hello";
 
-void reconnect_WIFI(){
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  // Wait until the connection has been confirmed before continuing
-  while (WiFi.status() != WL_CONNECTED) {
-    // to test 1s , otherwise 500ms
-    delay(1000);
-    Serial.print(".");
-  }
-}
+  char output[128];
+  StaticJsonDocument<128> message;
 
-void connections(){
-  if(WiFi.status() != WL_CONNECTED){
-    strip.setBrightness(0);
-    strip.setLedColorData(0, 255, 0, 0);
-    Serial.println("connection lost to WIFI");
-    reconnect_WIFI();
-  }
-  if (!client.connected()) {
-    strip.setBrightness(10);
-    strip.setLedColorData(0, 0, 0, 255);
-    reconnect_MQTT();
-  }
-  else{
-    strip.setLedColorData(0, 0, 255, 0);
-  }
-  strip.show();
+  message["ip"] = WiFi.localIP();
+  message["id"] = generate_client_id();
+
+  serializeJson(message, output);
+
+  Serial.println("Publishing hello message");
+  mqttClient.publish(helloTopic.c_str(), output);
 }
 
 void setup() {
+  Serial.begin(115200);
+
+  // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
+
+  // Configure RGB Led
   strip.begin();
   strip.setBrightness(10);
-  strip.setLedColorData(0,255,0,0);
-  strip.show();
-  // begin serial port
-  Serial.begin (9600);
-  // configure the trigger pin to output mode
-  pinMode(TRIG_PIN, OUTPUT);
-  // configure the echo pin to input mode
+  status_no_communications();
+
+  // Configure HS SR04 Ultrasonic Sensor Module pins
+  pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-  connect_WIFI_MQTT();
-  client.setCallback(callback);
-  // Status and mac 
-  const char * mac = WiFi.macAddress().c_str();
-  client.publish(devices_topic, mac);
-  //Callback for Subscribing 
-  
-  client.subscribe(sensor_topic_status);
-  Serial.println(sensor_topic_status);
-  //Serial.println(clientID);
+
+  Serial.println("Starting firmware of ESP32-C3 Sonic Module ...");
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+
+  connect_to_wifi();
+  connect_to_mqtt_broker();
+  publish_hello_package();
+
+  // mqttClient.setCallback(callback);
 }
 
 void loop() {
-  //checks connections to mqtt broker and WIFI
-  connections();
- // IF STATUS is true we start sending
-  if(Status == true){
-        // generate 10-microsecond pulse to TRIG pin
-      digitalWrite(TRIG_PIN, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(TRIG_PIN, LOW);
+//   //checks connections to mqtt broker and WIFI
+//   connections();
+//  // IF STATUS is true we start sending
+//   if(Status == true){
+//         // generate 10-microsecond pulse to TRIG pin
+//       digitalWrite(TRIG_PIN, HIGH);
+//       delayMicroseconds(10);
+//       digitalWrite(TRIG_PIN, LOW);
     
-      // measure duration of pulse from ECHO pin
-      duration_us = pulseIn(ECHO_PIN, HIGH);
+//       // measure duration of pulse from ECHO pin
+//       duration_us = pulseIn(ECHO_PIN, HIGH);
        
-      // calculate the distance
-      // calculate the distance limit between 0 an 1 
-      distance_cm = (0.017 * duration_us) * 0.01; // Beperken tussen 0 en 1
+//       // calculate the distance
+//       // calculate the distance limit between 0 an 1 
+//       distance_cm = (0.017 * duration_us) * 0.01; // Beperken tussen 0 en 1
       
-      if (distance_cm < 1) {
-        client.publish(sensor_topic, String(distance_cm).c_str());
-        ptr_distance_save = &distance_cm;
-        //Serial.println(sensor_topic);
-      }
-      else{
-        client.publish(sensor_topic, String(1).c_str());
-        //Serial.println(sensor_topic);
-      }
-   }
+//       if (distance_cm < 1) {
+//         mqttClient.publish(sensor_topic, String(distance_cm).c_str());
+//         ptr_distance_save = &distance_cm;
+//         //Serial.println(sensor_topic);
+//       }
+//       else{
+//         mqttClient.publish(sensor_topic, String(1).c_str());
+//         //Serial.println(sensor_topic);
+//       }
+//    }
 
-client.loop();
+// mqttClient.loop();
 }
